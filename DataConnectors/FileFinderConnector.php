9e10b7ac6b5ad0de265d42030e34f06e664dbb33
Andrej Kabachnik <andrej_ka@gmx.de>
2017-03-21 09:20:09 +0100

    FIX removed ignore_path_not_found_errors removed again

--- DataConnectors/FileFinderConnector.php
+++ DataConnectors/FileFinderConnector.php
@@ -6,15 +6,13 @@ use exface\Core\DataConnectors\TransparentConnector;
 use exface\Core\Interfaces\DataSources\DataQueryInterface;
 use exface\Core\Exceptions\DataSources\DataConnectionQueryTypeError;
 use exface\Core\Exceptions\DataSources\DataQueryFailedError;
-use exface\Core\DataTypes\BooleanDataType;
 
 class FileFinderConnector extends TransparentConnector {
 	private $base_path = null;
 	private $use_vendor_folder_as_base = false;
-	private $ignore_path_not_found_errors = true;
-	
+
 	/**
-	 * 
+	 *
 	 * {@inheritDoc}
 	 * @see \exface\Core\CommonLogic\AbstractDataConnector::perform_connect()
 	 */
@@ -23,21 +21,21 @@ class FileFinderConnector extends TransparentConnector {
 			$this->set_base_path($this->get_workbench()->filemanager()->get_path_to_base_folder());
 		} elseif ($this->get_use_vendor_folder_as_base() != false){
 			$this->set_base_path($this->get_workbench()->filemanager()->get_path_to_vendor_folder());
-		} 
+		}
 		return;
-	}	
+	}
 
 	/**
-	 * 
+	 *
 	 * {@inheritDoc}
 	 * @see \exface\Core\CommonLogic\AbstractDataConnector::perform_query()
-	 * 
+	 *
 	 * @param FileFinderDataQuery
 	 * @return FileFinderDataQuery
 	 */
 	protected function perform_query(DataQueryInterface $query) {
 		if (!($query instanceof FileFinderDataQuery)) throw new DataConnectionQueryTypeError($this, 'DataConnector "' . $this->get_alias_with_namespace() . '" expects an instance of FileFinderDataQuery as query, "' . get_class($query) . '" given instead!', '6T5W75J');
-		
+
 		$paths = array();
 		// Prepare an array of absolut paths to search in
 		foreach ($query->get_folders() as $path){
@@ -47,28 +45,17 @@ class FileFinderConnector extends TransparentConnector {
 				$paths[] = $path;
 			}
 		}
-		
+
 		// If the query does not have a base path, use the base path of the connection
 		if (!$query->get_base_path()){
 			$query->set_base_path($this->get_base_path());
 		}
-		
+
 		// If no paths could be found anywhere (= the query object did not have any folders defined), use the base path
 		if (count($paths) == 0){
 			$paths[] = $query->get_base_path();
 		}
-		
-		if ($this->get_ignore_path_not_found_errors()){
-			foreach ($paths as $nr => $path){
-				if (!is_dir($path)){
-					unset($paths[$nr]);
-				}
-			}
-			if (count($paths) == 0){
-				return $query;
-			}
-		}
-		
+
 		// Perform the search. This will fill the file and folder iterators in the finder instance. Thus, the resulting
 		// files will be available through foreach($query as $splFileInfo) etc.
 		try {
@@ -77,20 +64,20 @@ class FileFinderConnector extends TransparentConnector {
 			throw new DataQueryFailedError($query, "Data query failed!", null, $e);
 			return array();
 		}
-		
+
 		return $query;
 	}
-	  
+	 
 	public function get_base_path() {
 		return $this->base_path;
 	}
-	
+
 	/**
 	 * Sets the base path for the connection. If a base path is defined, all data addresses will be resolved relative to that path.
-	 * 
+	 *
 	 * @uxon-property base_path
 	 * @uxon-type string
-	 * 
+	 *
 	 * @param unknown $value
 	 * @return \exface\FileSystemConnector\DataConnectors\FileFinderConnector
 	 */
@@ -102,49 +89,29 @@ class FileFinderConnector extends TransparentConnector {
 		}
 		return $this;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @return boolean
 	 */
 	public function get_use_vendor_folder_as_base() {
 		return $this->use_vendor_folder_as_base;
 	}
-	
+
 	/**
-	 * Set to TRUE to use the current vendor folder as base path. 
+	 * Set to TRUE to use the current vendor folder as base path.
 	 * All data addresses in this conneciton will then be resolved relative to the vendor folder.
-	 * 
+	 *
 	 * @uxon-property base_path
 	 * @uxon-type boolean
-	 * 
+	 *
 	 * @param boolean $value
 	 * @return \exface\FileSystemConnector\DataConnectors\FileFinderConnector
 	 */
 	public function set_use_vendor_folder_as_base($value) {
 		$this->use_vendor_folder_as_base = \exface\Core\DataTypes\BooleanDataType::parse($value);
 		return $this;
-	} 
-	
-	public function get_ignore_path_not_found_errors() {
-		return $this->ignore_path_not_found_errors;
 	}
-	
-	/**
-	 * Set to TRUE to treat non-existant folders as empty folders. FALSE (default) will produce an error if the path does not exist.
-	 * 
-	 * @uxon-property ignore_path_not_found_errors
-	 * @uxon-type boolean
-	 * 
-	 * @param boolean $value
-	 * @return \exface\FileSystemConnector\DataConnectors\FileFinderConnector
-	 */
-	public function set_ignore_path_not_found_errors($value) {
-		$this->ignore_path_not_found_errors = BooleanDataType::parse($value);
-		return $this;
-	}
-	
-	  
-  
+
 }
 ?>
