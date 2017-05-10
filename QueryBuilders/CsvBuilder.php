145f64b582be35f7f3ae0c392430d503641c9ded
tomdom <tvwalter@web.de>
2017-05-10 12:26:18 +0200

    NEW CsvBuilder based on league/csv
    * CsvBuilder based on league/csv
    
    * "league/csv": "^8.0" added to composer.json
    
    * DailyLogfileCvsBuilder shows actual log of daily limited logs

--- QueryBuilders/CsvBuilder.php
+++ QueryBuilders/CsvBuilder.php
@@ -3,6 +3,8 @@
 use exface\Core\CommonLogic\QueryBuilder\AbstractQueryBuilder;
 use exface\Core\CommonLogic\AbstractDataConnector;
 use exface\FileSystemConnector\FileContentsDataQuery;
+use League\Csv\Reader;
+use SplFileObject;
 
 /**
  * A query builder to read CSV files.
@@ -60,48 +62,59 @@ class CsvBuilder extends AbstractQueryBuilder {
 	 * @see \exface\Core\CommonLogic\QueryBuilder\AbstractQueryBuilder::read()
 	 */
 	public function read(AbstractDataConnector $data_connection = null){
-		$result_rows = array();
 		$query = $this->build_query();
 		if (is_null($data_connection)){
 			$data_connection = $this->get_main_object()->get_data_connection();
 		}
 		
 		$data_connection->query($query);
-		
+
 		$field_map = array();
 		foreach ($this->get_attributes() as $qpart){
 			$field_map[$qpart->get_alias()] = $qpart->get_data_address();
 		}
-		
+
+		// configuration
 		$delimiter = $this->get_main_object()->get_data_address_property('DELIMITER') ? $this->get_main_object()->get_data_address_property('DELIMITER') : ',';
-		
-		if (($handle = fopen($query->get_path_absolute(), "r")) !== FALSE) {
-			while (($data = fgetcsv($handle, 1000, $delimiter, "'")) !== FALSE) {
-				$result_row = array();
-				foreach ($field_map as $fld => $col_no){
-					$result_row[$fld] = $data[$col_no];
-				}
-				$result_rows[] = $result_row;
-			}
-			fclose($handle);
+		$enclosure = $this->get_main_object()->get_data_address_property('ENCLOSURE') ? $this->get_main_object()->get_data_address_property('ENCLOSURE') : ',';
+
+		// prepare filters
+		foreach ($this->get_filters()->get_filters() as $qpart){
+			$qpart->set_alias($field_map[$qpart->get_alias()]); // use numeric alias since league/csv filter on arrays with numeric indexes
+			$qpart->set_apply_after_reading(true);
 		}
-				
-		
-		$this->set_result_total_rows(count($result_rows));
-		
-		$result_rows = $this->apply_filters($result_rows);
+
+		// prepare reader
+		$csv = Reader::createFromPath(new SplFileObject($query->get_path_absolute()));
+		$csv->setDelimiter($delimiter);
+		$csv->setEnclosure($enclosure);
+
+		// column count
+		$colCount = count($csv->fetchOne());
+
+		// add filter based on "normal" filtering
+		$filtered = $csv;
+		$filtered = $filtered->addFilter(function($row) {
+			return parent::apply_filters(array($row));
+		});
+
+		// pagination
+		$filtered->setOffset($this->get_offset());
+		$filtered->setLimit($this->get_limit());
+
+		$assocKeys = $this->getAssocKeys($colCount, $field_map);
+		$result_rows = $filtered->fetchAssoc($assocKeys);
+		$result_rows = iterator_to_array($result_rows);
+
+		// row count
+		$rowCount = $this->getRowCount($query->get_path_absolute(), $delimiter, $enclosure);
+		$this->set_result_total_rows($rowCount);
+
+		// sorting
 		$result_rows = $this->apply_sorting($result_rows);
-		$result_rows = $this->apply_pagination($result_rows);		
 		
 		$this->set_result_rows($result_rows);
 		return $this->get_result_total_rows();
-	}
-	
-	public function apply_filters($row_array){
-		foreach ($this->get_filters()->get_filters() as $qpart){
-			$qpart->set_apply_after_reading(true);
-		}
-		return parent::apply_filters($row_array);
 	}
 	
 	public function apply_sorting($row_array){
@@ -109,6 +122,47 @@ class CsvBuilder extends AbstractQueryBuilder {
 			$qpart->set_apply_after_reading(true);
 		}
 		return parent::apply_sorting($row_array);
+	}
+
+	protected function getAssocKeys($colCount, $field_map) {
+		$keys = array_flip($field_map);
+
+		$assocKeys = array();
+		for ($i = 0; $i < $colCount; $i++) {
+			if (isset($keys[$i]))
+				$assocKeys[$keys[$i]] = $keys[$i];
+			else
+				$assocKeys[$i] = '- unused' . $i;    // unique value, not used by query
+		}
+
+		return $assocKeys;
+	}
+
+	/**
+	 * Returns the row count after filtering the CSV.
+	 * This has to be done on a separate CSV object. Otherwise the complete row count is returned instead of the
+	 * filtered count.
+	 *
+	 * @param string $path path to CSV file
+	 * @param string $delimiter delimiter character
+	 * @param string $enclosure enclosure character
+	 *
+	 * @return int row count after filtering
+	 */
+	private function getRowCount($path, $delimiter, $enclosure) {
+		$csv = Reader::createFromPath(new SplFileObject($path));
+		$csv->setDelimiter($delimiter);
+		$csv->setEnclosure($enclosure);
+
+		// add filter based on "normal" filtering
+		$filtered = $csv;
+		$filtered = $filtered->addFilter(function($row) {
+			return parent::apply_filters(array($row));
+		});
+
+		return $filtered->each(function ($row) {
+			return true;
+		});
 	}
 }
 ?>
